099ec42e7cded2227dcef00339875eedb9f228ff
drenialj <dreni.alj@gmail.com>
2025-03-26 01:14:40 +0100

    Update Next.js version and fix theme provider

--- Desktop/taxuresaas/src/components/theme-provider.tsx
+++ Desktop/taxuresaas/src/components/theme-provider.tsx
@@ -13,6 +13,7 @@ export function ThemeProvider({ children }: ThemeProviderProps) {
       attribute="class"
       defaultTheme="system"
       enableSystem
+      disableTransitionOnChange
     >
       {children}
     </NextThemesProvider>
